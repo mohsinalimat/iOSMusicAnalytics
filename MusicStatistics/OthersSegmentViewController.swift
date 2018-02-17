ca9dcbbf7db8a94e759daa6535c8b345de7a7a8a
Jing Wei Li <ljw9801055@gmail.com>
2018-02-16 23:58:41 -0500

    Started Segmented-Control Switching of MVC in Others

---
+++ MusicStatistics/OthersSegmentViewController.swift
@@ -0,0 +1,38 @@
+//
+//  OthersSegmentViewController.swift
+//  MusicStatistics
+//
+//  Created by Jing Wei Li on 2/16/18.
+//  Copyright © 2018 Jing Wei Li. All rights reserved.
+//
+
+import UIKit
+
+class OthersSegmentViewController: UIViewController {
+
+    @IBOutlet weak var playlistView: UIView!
+    @IBOutlet weak var artistView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
